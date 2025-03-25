cfb9ef05fd33de33b9cb9243c40d1eac92c62580
unknown <diego.automacao137@gmail.com>
2025-03-24 21:22:43 -0300

    Implementação do banco de dados

--- src/database.js
+++ src/database.js
@@ -1,27 +1,73 @@
-import {generate, parse} from "csv"
+import fs from "node:fs/promises"
+
+const databasePath = new URL("../db.json", import.meta.url)
 
 export class Database{
 
-    database = {}
+    #database = {}
 
     constructor(){
-        console.log(parse())
-        console.log(generate())
+       
+        fs.readFile(databasePath, "urf8").then(data => {
+
+            this.#database = JSON.parse(data)
+
+        }).catch(() =>{
+
+            this.#persist()
+        })
+    }
+
+    #persist() {
+
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
     }
 
     select(table, search){
+
+        let data = this.#database[table] ?? []
+
+        if(search){}
+
+        return data
 
     }
 
     insert(table, data) {
 
+        if(Array.isArray(this.#database[table])){
+
+            this.#database[table].push(data)
+        }else{
+
+            this.#database[table] = [data]
+        }
+
+        this.#persist()
+
     }
 
     update(table, id, data){
 
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if(rowIndex > -1) {
+
+            this.#database[table][rowIndex] = {id, ...data}
+            this.#persist()
+        }
+
     }
 
     delete(table, id) {
+
+        const rowIndex = this.#database[table].findIndex(row => row.id = id)
+
+        if(rowIndex > -1) {
+
+            this.#database[table].splice(rowIndex, 1)
+            this.#persist()
+        }
 
     }
 
